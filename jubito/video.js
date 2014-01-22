567c868f2469aadf84a69303a520d537732c72a5
cympfh@NII <cympfh@gmail.com>
2014-01-23 03:38:36 +0900

    消されるコミット

--- jubito/video.js
+++ jubito/video.js
@@ -2,7 +2,7 @@ var id = "EK0Z6PDBB-E"
 
 function video_init() {
   swfobject.embedSWF(
-      "http://www.youtube.com/v/"+id+"?enablejsapi=1&autoplay=1&playerapiid=player&wmode=transparent"
+      "http://www.youtube.com/v/"+id+"?enablejsapi=1&autoplay=1&playerapiid=player"
     , "video", "480", "360", "8", null, null
     , { allowScriptAccess: "always" }, { id: "player" }); 
 }
