bf8d3b5a7933b57b6c22d268c46e23f47874c004
cympfh <cympfh@gmail.com>
2014-05-12 02:00:43 +0900

    そう…あの日おなじ夢を描いたんだ

--- taglibro/index.html.h
+++ taglibro/index.html.h
@@ -1,44 +1,44 @@
 <title>taglibro</title>
 <script src='./js/openarticle.js'></script>
-<body onload='main()'>
-<div id='article'></div>
-<a href=http://is.gd/mC4zzr>on github.com</a>
 <style>
   a {
     display: block;
-    width: 8em;
-    padding: 5px;
-    margin: 0px;
+    padding: 0;
+    margin: 0;
     color: black;
-    font-size: 1em;
     text-decoration: none;
   }
 
   /* menu for each articles */
-  div {
+  div.dates {
+    font-size: 1em;
+    width: 30%;
     float: left;
-    width: 8em;
+  }
+  div.black {
+    width: 25%;
+    float: left;
+  }
+  div.red {
+    width: 25%;
+    float: left;
   }
   div.black a {
     color: #f0f0f0;
     background-color: #404040;
   }
-  div.black a:visited {
-    color: #ffd0d0;
-  }
   div.red a {
     color: #404040;
     background-color: #f0f0f0;
   }
-  div.red a:visited {
-    color: #773030;
-  }
+  div.black a:visited { color: #ffd0d0; }
+  div.red a:visited { color: #773030; }
 
   /* preview window */
   div#article {
     float: right;
     display: none;
-    width: 48em;
+    width: 66%;
   }
   iframe {
     padding: 0;
@@ -51,4 +51,6 @@
     border-left: 1px black solid;
   }
 </style>
-<body>
+<body onload='main()'>
+<div class='article' id='article'></div>
+<div class='dates'><a href=http://is.gd/mC4zzr>on github.com</a>
