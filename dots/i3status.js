ab83d79465bd11dd5436283b3e7318f8020df6e9
cympfh <cympfh@gmail.com>
2014-06-10 15:06:35 +0900

    update i3status.js

--- dots/i3status.js
+++ dots/i3status.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
-var exec = require('child_process').exec;
+var exec = require('child_process').exec
+  , red = '#ff0000'
+  , yellow = '#ffff00'
+  , pink = '#f09090'
+  , white = '#ffffff'
+  , gray = '#404040'
+  ;
 
 function text(str, col) {
-  col = col || "#ffffff";
+  col = col || white;
   return { full_text: ''+str, color: col };
 }
 
@@ -33,18 +39,26 @@ function date() {
 }
 
 battery = (function () {
-  var result;
+  var result
+    , icons = "♩♪♫♬"
+    ;
+  var visualize = function (x) {
+    x = Math.ceil(x / 20); // [1, 2, 3, 4, 5]
+    if (x === 5) {
+      return text("+。:.ﾟ٩(๑>◡<๑)۶:.｡+ﾟ", pink);
+    } else if (x > 1) {
+      return text(icons[x-1], yellow);
+    } else {
+      return text(icons[0], red);
+    }
+  };
+
   return function () {
-    if (result && Math.random() < .3) return result;
-    exec('acpi', function(err, data) {
-      data = data.match(/\d+%/)[0];
-      data = parseInt(data, 10) / 20;
-      data =
-        [0,1,2,3,4]
-          .map(function (i) {
-            return (i < data) ? '#' : '/' })
-          .join('');
-      result = data;
+    if (result && Math.random() < .7) return result;
+    exec('acpi', function(er, ou) {
+      if (er) return;
+      var x = ou.match(/(\d+)%/)[1];
+      result = visualize(x);
     });
     return result;
   };
@@ -53,12 +67,37 @@ battery = (function () {
 coretemp = (function () {
   var result;
   return function () {
-    if (result && Math.random() < .2) return result;
+    if (result && Math.random() < .7) return result;
     exec('sensors | grep "Core 0" | cut -d "+" -f 2 | cut -c 1-4', function (err, data) {
       result = data.split('\n')[0] + '°C';
     });
     return result;
   };
+}());
+
+tenki = (function () {
+  var result
+    , icons = {}
+    , url = "http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp" // may change
+    ;
+
+  icons.Clouds = '曇';
+  // '☀☔☇'.split('')
+  
+  return function () {
+    if (result && Math.random() < .990) return result;
+    exec("curl \"" + url + "\" > /tmp/w.json", function (err) {
+      if (err) return;
+      var w = require('/tmp/w.json')
+        , t = w.weather[0].main;
+      if (t in icons) {
+        result = icons[t];
+      } else {
+        result = t;
+      }
+    });
+    return result;
+  }
 }());
 
 (function main() {
@@ -70,9 +109,10 @@ coretemp = (function () {
   function loop(interval) {
     var ls = [];
 
-    ls = ls.concat(text(coretemp(), '#ff0000'));
-    ls = ls.concat(text(battery(), '#ffff00'));
-    ls = ls.concat(text(date(), '#ffffff'));
+    ls = ls.concat(text(tenki(), gray));
+    ls = ls.concat(text(coretemp(), red));
+    ls = ls.concat(battery());
+    ls = ls.concat(text(date(), white));
 
     console.log('%j,', ls);
     setTimeout(loop, interval, interval);
