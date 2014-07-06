9681c75c95fa89157e8757804301aefa0e7c43bf
cympfh <cympfh@gmail.com>
2014-07-07 02:07:08 +0900

    2014年  7月  7日 月曜日 02:07:08 JST

---
+++ c++/src/include.cpp
@@ -0,0 +1,47 @@
+#ifndef stdc++
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <ccomplex>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <tuple>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
