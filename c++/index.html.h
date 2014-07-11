71fa65bed86c6f38a700fc00154ea2115a117dd0
cympfh <cympfh@gmail.com>
2014-07-12 00:25:05 +0900

    love iframe

--- c++/index.html.h
+++ c++/index.html.h
@@ -1,16 +1,26 @@
 <title>c++</title>
+<script src='../taglibro/js/openarticle.js'></script>
 <style>
-code {
-  background: white;
-  color: #a00;
-  padding: 0 10px;
-}
-code span.prompt {
-  color: red;
-  padding-right: 10px;
-}
 a {
   color: #402020;
 }
+/* preview window */
+div#article {
+  float: right;
+  display: none;
+  width: 66%;
+}
+iframe {
+  padding: 0;
+  position: fixed;
+  margin: 0;
+  padding: 0;
+  width: 62%;
+  height: 96%; /* for scroll bar */
+  border: 0;
+  border-left: 1px black solid;
+}
 </style>
-<body>
+<body onload='main()'>
+<div class='article' id='article'></div>
+<div class='dates'><input type='checkbox' id='check_if_iframe'>view on iframe<br>
