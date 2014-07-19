b7d0bced070e3a54c657c3eb43513428cac89c70
cympfh <cympfh@gmail.com>
2014-07-19 17:10:16 +0900

    MOD

--- c++/src/graph.prim.use.cpp
+++ c++/src/graph.prim.use.cpp
@@ -7,36 +7,17 @@ using namespace std;
 
 #define INF (1e9)
 #define EPS (1e-9)
-#define cons(a,b) (make_pair(a,b))
-#define car(a) (a.first)
-#define cdr(a) (a.second)
-#define cadr(a) (car(cdr(a)))
-#define cddr(a) (cdr(cdr(a)))
-#define trace(var) cout<<">>> "<<#var<<" = "<<var<<endl;
-
-typedef pair<int,int> P;
-
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,1,0,-1};
-
-template<class S, class T>
-ostream& operator<<(ostream& os, pair<S,T> p) {
-  os << '(' << car(p) << ", " << cdr(p) << ')';
-  return os;
-}
-template<class T>
-ostream& operator<<(ostream& os, vector<T> v) {
-  os << "(vector";
-  for (int i=0, len=v.size(); i<len; ++i) os << ' ' << v[i];
-  os << ')';
-  return os;
-}
 
 struct edge {
   int from, to, cost;
 };
 bool operator> (const edge& left, const edge& right) {
   return left.cost > right.cost;
+}
+
+ostream& operator<<(ostream& os, const edge& e) {
+  os<<"(edge "<<e.from<<' '<<e.to<<' '<<e.cost<<')';
+  return os;
 }
 
 #define MAXN 1000
