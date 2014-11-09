e817c301f9569491d23981e49e76007d5e01971c
cympfh <cympfh@gmail.com>
2014-11-09 22:18:41 +0900

    Sun Nov  9 22:18:41 JST 2014

--- c++/src/template.cpp
+++ c++/src/template.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep2(i,n,m) for(int i=(int)(n);i<(int)(m);++i)
-#define rep(i,n) rep2(i,0,n)
-#define iota(i,n,b,s) for(int i=int(b);i<int(b+s*n);i+=s)
+#define iota(i,n,b,s) for(int i=int(b);i!=int(b+s*n);i+=s)
+#define range_debug(i,n,m) (n>m?(n-m+1):(m-n+1))<<n<<(n>m?-1:1)
+#define range(i,n,m) iota(i,(n>m?(n-m+1):(m-n+1)),n,(n>m?-1:1))
+#define rep(i,n) iota(i,n,0,1)
 
 #define INF (1e9)
 #define EPS (1e-9)
@@ -14,27 +15,30 @@ using namespace std;
 #define cddr(a) (cdr(cdr(a)))
 #define trace(var) cout<<">>> "<<#var<<" = "<<var<<endl;
 
-typedef pair<int,int> P;
-typedef vector<int> vi;
- 
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,1,0,-1};
+typedef long long INTEGER;
+typedef double FLOAT;
 
 template<class S, class T>
 ostream& operator<<(ostream& os, pair<S,T> p) {
   os << '(' << car(p) << ", " << cdr(p) << ')';
   return os;
 }
+
 template<class T>
 ostream& operator<<(ostream& os, vector<T> v) {
-  os << "(vector";
-  for (int i=0, len=v.size(); i<len; ++i) os << ' ' << v[i];
-  os << ')';
+  os << v[0];
+  for (int i=1, len=v.size(); i<len; ++i) os << ' ' << v[i];
   return os;
 }
 
 int main() {
-  int n;
+
+  int n, m;
+  cin >> n >> m;
+
+  range (i, n, m) {
+    cout << i << endl;
+  }
 
   return 0;
 }
