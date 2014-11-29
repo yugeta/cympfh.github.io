281ec1067abc6ef5ec197ab33dea3c663d5abbfa
cympfh@NII <cympfh@gmail.com>
2014-11-29 16:36:38 +0900

    2014年 11月29日 土曜日 16時36分38秒 JST

--- c++/main.md
+++ c++/main.md
@@ -1,23 +1,23 @@
-## 参考
+# 参考
 
 - [C++ and STL reference](http://www.ccll.jp/cppreference/index.html)
 - [cpprefjp - C++ Library Reference](https://sites.google.com/site/cpprefjp/)
 - [１次元幾何ライブラリ - not's memo - TopCoder部](http://topcoder.g.hatena.ne.jp/not522/20130401/1364781205)
 
-## [鋳型](src/template.cc.html)
+# [鋳型](src/template.cc.html)
 
 - [要求の代替](src/include.cc.html)
 
-## [グラフ](src/graph.head.cc.html)
+# [グラフ](src/graph.head.cc.html)
 
-### 最短路
+## 最短路
 
 - [Dijkstra](src/graph.dij.cc.html)
     - [sample](src/graph.dij.use.cc.html)
 - [Warshall-Floyd](src/graph.wall.cc.html)
     - [sample](src/graph.wall.use.cc.html)
 
-### 最小全域木
+## 最小全域木
 
 - [Prim](src/graph.prim.cc.html)
     - [sample](src/graph.prim.use.cc.html)
@@ -25,24 +25,30 @@
 - [Kruskal](src/graph.kruskal.cc.html)
     - [sample](src/graph.kruskal.use.cc.html)
 
-### 最大流量
+## 最大流量
 
 - [Ford-Fulkerson](src/graph.maxflow.cc.html)
     - [sample](src/graph.maxflow.use.cc.html)
 
-## 幾何
+# 幾何
 
 - [平面テンプレート](src/geo.2d.cc.html)
     - [test](src/geo.2d.test.cc.html)
 
-## 集合
+# 集合
 
 - [Union-Find](src/set.union.find.cc.html)
-- [Union-Find](src/set.union.find.cc)
 
-## misc
+# 動的計画法
+
+## Knapsack problem
+
+- [0-1 knapsack](src/dp.knapsack.01.cc.html)
+- [0-1 knapsack](src/dp.knapsack.01.cc)
+
+# misc
 
 - [bit count](src/bit.count.cc.html)
 - [ツェラーの公式](src/misc.zeller.cc.html)
 
-## [テスト環境](src/test.html)
+# [テスト環境](src/test.html)
---
+++ c++/src/dp.knapsack.01.cc
@@ -0,0 +1,97 @@
+
+/*
+ * 0-1 knapsack problem
+ *
+ * Knapsack problem - Wikipedia
+ * http://en.wikipedia.org/wiki/Knapsack_problem
+ *
+ * solve with DP of cost
+ * O(N * C)
+ *
+ * where
+ *   N is the number of variety
+ *   C is the total capacity
+ */
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+#define iota(i,n,b,s) for(int i=int(b);i!=int((b)+(s)*(n));i+=(s))
+#define range(i,n,m) iota(i,(((n)>(m))?((n)-(m)+1):((m)-(n)+1)),(n),((n)>(m)?-1:1))
+#define rep(i,n) iota(i,(n),0,1)
+
+void debug(int*ar, int size) {
+  cerr << ar[0];
+  for (int i=1; i<size; ++i) {
+    cerr << ' ' << ar[i];
+  }
+  cerr << endl;
+}
+
+template<class T>
+ostream& operator<<(ostream& os, vector<T> v) {
+  os << v[0];
+  for (int i=1, len=v.size(); i<len; ++i) os << ' ' << v[i];
+  return os;
+}
+
+void solve(istream&);
+
+int main(){
+
+  /*
+   * number-of-variety (N)
+   * capacity (C)
+   * i-th-cost i-th-value (c[i], v[i])
+   */
+
+  stringstream sample1("\
+6\n\
+10\n\
+3 70\n\
+2 40\n\
+2 80\n\
+5 90\n\
+1 30\n\
+1 50");
+
+  solve(sample1);
+
+  return 0;
+}
+
+void
+solve(istream&is) {
+  int N, C;
+  is >> N >> C;
+
+  int c[N], v[N];
+
+  rep (i, N) {
+    is >> c[i];
+    is >> v[i];
+  }
+
+  // DP of cost
+  int dp[C+1];
+  vector<vector<int>> detail(C+1, {});
+
+  rep (k, C+1) dp[k] = 0;
+
+  rep (i, N) {
+    range (k, C-c[i], 0) {
+      int k2 = k + c[i];
+      if (dp[k2] < dp[k] + v[i]) {
+        dp[k2] = dp[k] + v[i];
+        detail[k2] = detail[k];
+        detail[k2].push_back(i);
+      }
+    }
+    debug(dp, C+1);
+  }
+  cout << dp[C] << endl;
+  cout << "内訳: " << detail[C] << endl;
+}
+ 
