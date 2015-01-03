b4110db9320b0dcc1ca7d98c71fed724ac6a0fc3
cympfh <cympfh@gmail.com>
2015-01-03 12:38:34 +0900

    Sat Jan  3 12:38:34 JST 2015

--- c++/src/misc.zeller.cc
+++ c++/src/misc.zeller.cc
@@ -4,27 +4,30 @@ enum Day {
   Mon, Tue, Wed, Thu, Fri
 };
 
-Day
-zeller(int y, int m, int d) {
+Day zeller(int y, int m, int d) {
+  if (m == 1 || m == 2) {
+    --y; m += 12;
+  }
   int Y = y % 100;
-  int c = int(y/100);
-  int gamma = -2 * c + int(c/4); // Gregorian (1582 <= y)
-  //int gamma = -c + 5; // Julian (4 <= y <= 1582)
+  int c = y / 100;
+  int gamma = 5 * c + c / 4; // Gregorian (1582 <= y)
+  //int gamma = 6 * c + 5; // Julian (4 <= y <= 1582)
   int h = d + int(26 * (m+1) / 10)
-    + Y + int(Y/4) + gamma;
+    + Y + Y / 4 + gamma;
   h %= 7;
   return Day(h);
 }
 
 string show_day (Day a) {
   switch (a) {
-    case Mon: return "Monday"; break;
-    case Tue: return "Tuesday"; break;
-    case Wed: return "Wednesday"; break;
-    case Thu: return "Thursday"; break;
-    case Fri: return "Friday"; break;
-    case Sat: return "Saturday"; break;
-    case Sun: return "Sunday"; break;
+    case Mon: return "Monday";
+    case Tue: return "Tuesday";
+    case Wed: return "Wednesday";
+    case Thu: return "Thursday";
+    case Fri: return "Friday";
+    case Sat: return "Saturday";
+    case Sun: return "Sunday";
+    default: return "Holiday";
   }
 }
 
