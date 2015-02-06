8dad456b8dd0c5dbcf92fade1c2d7aa58b7664ba
cympfh <cympfh@gmail.com>
2015-02-07 08:04:20 +0900

    Sat Feb  7 08:04:19 JST 2015

--- tool/neta/js/list.js
+++ tool/neta/js/list.js
@@ -92,6 +92,7 @@ list = [
   "B7xz7_NCQAA__mt.jpg",
   "B8MhmbvCUAIrgwp.jpg",
   "B8PX5V8CYAAzVvq.jpg",
+  "B9Mku_SCIAARUaE.jpg",
   "BaCJtUKCAAAWGX2.png:large.png",
   "BaD47MBIMArge.jpeg",
   "BaVHLoBCAAA4Yk_.png",
