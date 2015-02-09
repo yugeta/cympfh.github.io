bbecb6a7ee397f22145a280d4875d7a523169adf
cympfh <cympfh@gmail.com>
2015-02-09 10:01:22 +0900

    add taglibro/top.html and add the shortcut to the page at /index

--- index.html.h
+++ index.html.h
@@ -1,15 +1,25 @@
 <!-- vim: set ft=html: -->
-<title>cympfh</title>
-<link rel="stylesheet" type="text/css" href="css/b.css">
-<script src='./js/poster.js'></script>
+<!doctype html>
+<html>
+  <head>
+  <meta charset="UTF-8">
+  <title>cympfh</title>
+  <link rel="stylesheet" type="text/css" href="css/b.css">
+  <script src='./js/poster.js'></script>
+</head>
 <body>
   <img src='img/nekomezaka.gif' width='200px' style='position:absolute;top:0;right:0' onclick='nekome()'>
 
   <div id='poster'>
-    <img alt='icon' title='枚方&#x0A;cympfh&#x0A;ampeloss' src='./img/identicon.png' height='96px' onclick='show()'>
-    &nbsp;
-    <a class='aimg' href='http://twitter.com/cympfh'><img alt='twitter' src='./img/Twitter_logo_blue.png' height='60px'></a>
-    <a class='aimg' href='https://github.com/cympfh'><img alt='github' src='./img/GitHub-Mark-120px-plus.png' height='60px'></a>
+    <div class='icons'>
+      <img alt='icon' title='枚方&#x0A;cympfh&#x0A;ampeloss' src='./img/identicon.png' height='96px' onclick='show()'>
+      &nbsp;
+      <a class='aimg' href='http://twitter.com/cympfh'><img alt='twitter' src='./img/Twitter_logo_blue.png' height='60px'></a>
+      <a class='aimg' href='https://github.com/cympfh'><img alt='github' src='./img/GitHub-Mark-120px-plus.png' height='60px'></a>
+    </div>
+    <center>
+      <a class='shortcut' href='taglibro/top.html'>▼</a>
+  </center>
   </div>
 
   <div id='cont'>
