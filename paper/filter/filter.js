b27fbda6910eac648029564a5b84f3e6a8c7b79f
cympfh <cympfh@gmail.com>
2015-03-06 18:18:21 +0900

    paper/filter: re-make

--- paper/filter/filter.js
+++ paper/filter/filter.js
@@ -11,39 +11,59 @@ function filter(query, datum) {
   var k, i;
   var scores = {};
   var word, ls, j;
-  for (k = 2; k <= 5; ++k) {
-    for (i = 0; i < query.length - k; ++i) {
-      word = query.slice(i, i + k);
-      ls = datum[word];
-      if (ls) {
-        for (j = 0; j < ls.length; ++j) {
-          if (scores[ls[j]]) {
-            scores[ls[j]]++;
-          } else {
-            scores[ls[j]] = 1;
+
+  var queries =
+    query.trim().split(' ')
+    .map(function (l) { return l.trim(); })
+    .filter(function (l) { return !!l; });
+
+  queries.forEach(function (q) {
+
+    for (k = 2; k <= 5; ++k) {
+      for (i = 0; i < q.length - k; ++i) {
+        word = q.slice(i, i + k);
+        ls = datum[word];
+        if (ls) {
+          for (j = 0; j < ls.length; ++j) {
+            if (scores[ls[j]]) {
+              scores[ls[j]]++;
+            } else {
+              scores[ls[j]] = 1;
+            }
           }
         }
       }
     }
-  }
+
+  });
 
   if (scores.length === 0) {
     return [];
   }
 
   var sup = maximum(scores);
+  var threshold = sup * 0.10;
+  console.dir({ sup: sup, threshold: threshold });
   var ret = [];
   var id;
   for (id in scores) {
-    if (scores[id] > sup * 0.03) {
+    if (scores[id] > threshold) {
       ret.push(id);
       console.log('push', id, scores[id]);
-    }
-    else {
+    } else {
       console.log('sute', id, scores[id]);
     }
   }
 
   return ret;
 }
+
+if (!console) {
+  console = {
+    log: function () { return 0; },
+    warn: function () { return 0; },
+    dir: function () { return 0; }
+  };
+}
+
 
